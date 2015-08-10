fd5f71db23555cc3ecd7bcc026a35a28e41a5d32
Vladimir Vladimirov <vladimir.vladimirov@magnetic.com>
2015-08-09 21:15:28 -0400

    check ganglia packages on master and slave

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -8,6 +8,14 @@ rm -rf /mnt/ganglia/rrds/*
 mkdir -p /mnt/ganglia/rrds
 chown -R nobody:nobody /mnt/ganglia/rrds
 
+/root/spark-ec2/ganglia/check_packages.py
+
+pssh --inline \
+    --host "$SLAVES $OTHER_MASTER" \
+    --user root \
+    --extra-args "-t -t $SSH_OPTS" \
+    --timeout 0 \
+    "spark-ec2/ganglia/check_packages.py"
 
 # Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
 rmdir /var/lib/ganglia/rrds
