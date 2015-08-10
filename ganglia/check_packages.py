fcd83c1ec43bd3139ab9750201e74c079252b01c
Vladimir Vladimirov <vladimir.vladimirov@magnetic.com>
2015-08-09 20:51:30 -0400

    working version

--- ganglia/check_packages.py
+++ ganglia/check_packages.py
@@ -4,54 +4,96 @@ Script to check for exact ganglia and httpd package version installed
 If not found - install them
 If found but version doesn't match - remove and install correct versions
 """
-import yum
+
+# the following imports were copied from yum-cli/cli.py module - to make API work
+# there are some side effects that make YumBaseCli().getOptionsConfig() method work
+import sys
+sys.path.insert(0, '/usr/share/yum-cli')
+
+import os
+import os.path
+import sys
+import logging
+import time
+import errno
+
+from yum import Errors
+from yum import plugins
+from yum import logginglevels
+from yum import _
+from yum.i18n import to_unicode, utf8_width
+import yum.misc
+import cli
+from utils import suppress_keyboard_interrupt_message, show_lock_owner, exception2msg
 
 
-required_packages = [
-    ("httpd", "2.2.29"),
-    ("php", "5.3.29"),
-    ("ganglia", "3.3.7"),
-    ("ganglia-web", "3.3.7"),
-    ("ganglia-gmond", "3.3.7"),
-    ("ganglia-gmetad", "3.3.7"),
-]
+yb = cli.YumBaseCli()
+# point yum to older version of Amazon repo and disable updates repo to point yum to ganglia 3.3.7
+args = ["--releasever=2013.03", "--disablerepo=amzn-updates*", "--skip-broken", "list", "ganglia"]
+yb.getOptionsConfig(args)
 
-yb = yum.YumBase()
 installed_packages_dict = dict([(p.name, p) for p in yb.rpmdb.returnPackages()])
 
 operations_log = []
 
-def remove_package(package_name):
-    yb.remove(name=package_name)
+def remove_package(**kwargs):
+    """
+
+    :param kwargs: dict
+        accepted keys: name and version, pattern
+    :return:
+    """
+    yb.remove(**kwargs)
     yb.resolveDeps()
     yb.buildTransaction()
     yb.processTransaction()
 
 
-def install_package(package_name, version):
-    yb.install(name=package_name, version=version)
+def install_package(**kwargs):
+    """
+
+    :param kwargs: dict
+        accepted keys: name and version, pattern
+    :return:
+    """
+    yb.install(**kwargs)
     yb.resolveDeps()
     yb.buildTransaction()
     yb.processTransaction()
 
-# uninstall wrong versions
-for package_name, required_version in required_packages:
-    if package_name in installed_packages_dict:
-        installed_package = installed_packages_dict[package_name]
-        if not installed_package.version.startswith(required_version):
-            print "Removing package: %s %s != %s" % (package_name, installed_package.version, required_version)
-            remove_package(package_name)
-            operations_log.append("Removed package: %s %s != %s" % (package_name, installed_package.version, required_version))
 
-installed_packages_dict = dict([(p.name, p) for p in yb.rpmdb.returnPackages()])
+# check httpd version
+# if doesn't match uninstall and install httpd2.2
 
-# install packages
-installed_packages_dict = dict([(p.name, p) for p in yb.rpmdb.returnPackages()])
-for package_name, required_version in required_packages:
-    if package_name not in installed_packages_dict:
-        print "installing package %s %s" % (package_name, required_version)
-        install_package(package_name, required_version)
-        operations_log.append("Installed package: %s %s" % (package_name, required_version))
+if "httpd" in installed_packages_dict:
+    httpd_po = installed_packages_dict["httpd"]
+    if httpd_po:
+        if not httpd_po.version.startswith("2.2."):
+            remove_package(pattern="httpd*")
+            install_package(pattern="httpd*2.2.*")
+else:
+    install_package(pattern="httpd*2.2.*")
+
+# check ganglia version
+# if doesn't match - uninstall php* and ganglia*
+
+if "ganglia" in installed_packages_dict:
+    ganglia_po = installed_packages_dict["ganglia"]
+    if not ganglia_po.version.startswith("3.3."):
+        operations_log.append("Invalid ganglia version: %s != %s" % (ganglia_po.version, "3.3.*"))
+        print "Removing packages: php*"
+        remove_package(pattern="php*")
+        operations_log.append("Removed packages: %s" % ("php*"))
+        print "Removing packages: ganglia*"
+        remove_package(pattern="ganglia*")
+        operations_log.append("Removed packages: ganglia*")
+        print "Installing ganglia"
+        install_package(pattern="ganglia*")
+        operations_log.append("Installed package: ganglia")
+else:
+    install_package(pattern="ganglia*")
+    operations_log.append("installed ganglia packages")
+
 
 print "Report:"
 if operations_log:
